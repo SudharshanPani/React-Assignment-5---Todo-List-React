2f8156198260190b3e8f9e44deec2ee66b6a970c
Sudharshan Pani G <67590792+SudharshanPani@users.noreply.github.com>
2021-04-07 16:46:57 +0530

    Add files via upload

--- src/components/App.js
+++ src/components/App.js
@@ -1,15 +1,53 @@
-import React from "react";
+import React, { useState } from "react";
+import ListItem from "./ListItem";
 import "./../styles/App.css";
 
-function App() 
-{
-	return (
-	<div id="main">
-	//Do not alter main div
-	//Please do not alter the functional component as tests depend on the type of component.
-	</div>
-	);
-}
+function App() {
+  const [items, setItems] = useState([]);
+  const [newItem, setNewItem] = useState("");
 
+  const newTask = (evt) => {
+    setNewItem(evt.target.value);
+  };
+  const saveItem = () => {
+    if (newItem.length > 0) {
+      items.push(newItem);
+      setItems([...items]);
+      setNewItem("");
+    }
+  };
+  const removeItem = (idx) => {
+    items.splice(idx, 1);
+    setItems([...items]);
+  };
+  const updateItem = (idx, value) => {
+    items[idx] = value;
+    setItems([...items]);
+  };
+  return (
+    <div id="main">
+      <h1>To-do List</h1>
+      <textarea
+        id="task"
+        type="text"
+        placeholder="New Item"
+        value={newItem}
+        onChange={newTask}
+      ></textarea>
+      <button id="btn" onClick={saveItem}>
+        Add Task
+      </button>
+      {items.map((item, idx) => (
+        <ListItem
+          item={item}
+          key={idx}
+          index={idx}
+          removeItem={removeItem}
+          updateItem={updateItem}
+        />
+      ))}
+    </div>
+  );
+}
 
 export default App;
---
+++ src/components/ListItem.js
@@ -0,0 +1,48 @@
+import React, { useState } from "react";
+
+import "./../styles/App.css";
+
+function ListItem(props) {
+  var update = "";
+  const [editVal, setEditVal] = useState(true);
+  const edit = () => {
+    setEditVal(false);
+  };
+  const deleteTask = () => {
+    props.removeItem(props.index);
+  };
+  const updateTask = (evt) => {
+    update = evt.target.value;
+  };
+  const saveTask = () => {
+    if (update.length > 0) {
+      props.updateItem(props.index, update);
+      setEditVal(true);
+      update = "";
+    }
+  };
+  return (
+    <div className="list">
+      {props.index + 1}.{props.item}
+      {editVal ? (
+        <>
+          <button className="edit" onClick={edit}>
+            Edit
+          </button>
+          <button className="delete" onClick={deleteTask}>
+            Delete
+          </button>
+        </>
+      ) : (
+        <>
+          <textarea className="editTask" onChange={updateTask}></textarea>
+          <button className="saveTask" onClick={saveTask}>
+            Save
+          </button>
+        </>
+      )}
+    </div>
+  );
+}
+
+export default ListItem;
